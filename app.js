f0a91e7e2a524dc972d115007af045a2b58b8337
Santiago Marin Suarez <santiago.marin@sckans.edu>
2022-12-02 15:02:36 -0600

    fixed nodemon

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.listen(3000, function(){
+    console.log("server is running on port " + 3000);
+});
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
